959d444642b863d379c00052951ba13ba99b3a4c
JesusPerez <yesper233@gmail.com>
2026-02-28 19:06:21 -0600

    Update portfolio images and text

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,13 +11,17 @@ const projectsData: ProjectData[] = [
         id: 1,
         title: 'SonaPlay',
         category: 'App movil reproductor de musica para Android',
-        image: 'imagenes/sonaplay2.webp',
+        image: 'imagenes/sonaplay/sonaplay2.webp',
         description: 'Aplicación móvil Android, un reproductor de música con una interfaz fluida y una experiencia de usuario intuitiva, sin anuncios.',
         fullDescription: 'SonaPlay es una aplicación nativa diseñada para ofrecer la mejor experiencia auditiva local en dispositivos Android. \n\n Cansado de los anuncion decidi crear un reproductor de música para Android que no contenga anuncios, fue un proyecto en el cual desarrolle mis habilidades en Flutter, Dart, Kotlin y Firebase con Crashlytics',
         link: 'https://www.mediafire.com/file/b0u8i79c52h4i9f/SonaPlay.apk/file',
         gallery: [
-            '/imagenes/sonaplay.webp',
-            '/imagenes/sonaplay2.webp'
+            '/imagenes/sonaplay/principal.webp',
+            '/imagenes/sonaplay/playlist.webp',
+            '/imagenes/sonaplay/playlist_info.webp',
+            '/imagenes/sonaplay/favoritos.webp',
+            '/imagenes/sonaplay/buscador.webp',
+            '/imagenes/sonaplay/reproductor.webp'
         ],
         tech: [
             { name: 'Flutter', icon: '/tecnologias/Flutter.webp' },
@@ -30,12 +34,41 @@ const projectsData: ProjectData[] = [
         id: 2,
         title: 'Recorrido virtual de una empresa',
         category: 'Recorrido virtual 360°',
-        image: 'imagenes/fiesta.webp',
+        image: 'imagenes/fiesta/fiesta.webp',
         description: 'Recorrido virtual 360° de una empresa, permitiendo a los usuarios explorar las instalaciones de forma interactiva.',
         fullDescription: 'Este proyecto consistió en levantar un entorno panorámico 360° interactivo para una empresa local. \n\nEl sistema permite la libre visualización inmersiva de los espacios corporativos directamente desde el navegador, empleando HTML5 y CSS3 puros renderizados encima de la librería Marzipano en JavaScript, lo que garantiza desempeño en tiempo real y compatibilidad alta entre plataformas.',
         link: 'https://jesusfernandopch.github.io/CTuristico/',
         gallery: [
-            '/imagenes/fiesta.webp'
+            '/imagenes/fiesta/fiesta.webp',
+            '/imagenes/fiesta/fiesta1.webp',
+            '/imagenes/fiesta/fiesta2.webp',
+            '/imagenes/fiesta/fiesta3.webp',
+            '/imagenes/fiesta/fiesta4.webp',
+            '/imagenes/fiesta/fiesta5.webp',
+            '/imagenes/fiesta/fiesta6.webp'
+
+        ],
+        tech: [
+            { name: 'HTML5', icon: '/tecnologias/HTML5-logo.webp' },
+            { name: 'CSS3', icon: '/tecnologias/css2.webp' },
+            { name: 'JavaScript', icon: '/tecnologias/jav1.webp' },
+            { name: 'Marzipano', icon: '/tecnologias/marzipano.webp' }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Minimalist Interactive Portfolio',
+        category: 'Résumé',
+        image: 'imagenes/minimalist/Inicio.webp',
+        description: 'Crea tu propio Résumé de forma interactiva para sistemas ATS.',
+        fullDescription: 'Este proyecto consistió en crear un Résumé de forma interactiva para sistemas ATS. \n\nHoy en dia tener un cv capaz de pasar los filtros de los sistemas ATS es fundamental para conseguir empleo, por lo que decidí crear un Résumé de forma interactiva para sistemas ATS.',
+        link: 'https://jesusfernandopch.github.io/CTuristico/',
+        gallery: [
+            '/imagenes/minimalist/demo.webp',
+            '/imagenes/minimalist/Inicio.webp',
+            '/imagenes/minimalist/final.webp',
+            '/imagenes/minimalist/imprimir.webp',
+            '/imagenes/minimalist/resultado.webp'
         ],
         tech: [
             { name: 'HTML5', icon: '/tecnologias/HTML5-logo.webp' },
